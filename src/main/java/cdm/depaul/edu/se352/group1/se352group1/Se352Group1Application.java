43c9a31e3cd1d026e7fd52ee5d9ef0da49900945
CamLav24 <121827092+CamLav24@users.noreply.github.com>
2024-04-14 19:39:47 -0500

    Setting up env

---
+++ src/main/java/cdm/depaul/edu/se352/group1/se352group1/Se352Group1Application.java
@@ -0,0 +1,13 @@
+package cdm.depaul.edu.se352.group1.se352group1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se352Group1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Se352Group1Application.class, args);
+    }
+
+}
---
+++ src/test/java/cdm/depaul/edu/se352/group1/se352group1/Se352Group1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cdm.depaul.edu.se352.group1.se352group1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se352Group1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

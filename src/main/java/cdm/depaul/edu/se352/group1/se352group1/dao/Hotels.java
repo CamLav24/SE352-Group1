0d37170e1823ddc8e17f6c45a434deb0967f8438
lilRedBb <jerrytemred@gmail.com>
2024-05-05 21:22:55 -0500

    jerry add dao,dto,mapper,resources

---
+++ src/main/java/cdm/depaul/edu/se352/group1/se352group1/dao/Hotels.java
@@ -0,0 +1,65 @@
+package cdm.depaul.edu.se352.group1.se352group1.dao;
+
+import java.math.BigDecimal;
+
+public class Hotels {
+    private Integer hotelId;
+
+    private String name;
+
+    private Integer locationId;
+
+    private BigDecimal price;
+
+    private Integer roomsLeft;
+
+    private String description;
+
+    public Integer getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(Integer hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(Integer locationId) {
+        this.locationId = locationId;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getRoomsLeft() {
+        return roomsLeft;
+    }
+
+    public void setRoomsLeft(Integer roomsLeft) {
+        this.roomsLeft = roomsLeft;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+}
---
+++ src/main/java/cdm/depaul/edu/se352/group1/se352group1/dto/GetHotelListReqBO.java
@@ -0,0 +1,4 @@
+package cdm.depaul.edu.se352.group1.se352group1.dto;
+
+public class GetHotelListReqBO {
+}
